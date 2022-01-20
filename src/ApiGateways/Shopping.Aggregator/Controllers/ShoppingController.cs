555f08706157641b2328391c964a923487b4c82d
m.moazen <m.moazen@muth-sev.de>
2022-01-20 10:22:11 +0100

    Shopping.Aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+
+namespace Shopping.Aggregator.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ShoppingController : ControllerBase
+    {
+        private readonly IBasketService basketService;
+        private readonly ICatalogService catalogService;
+        private readonly IOrderService orderService;
+
+        public ShoppingController(ICatalogService catalogService, IBasketService basketService,
+            IOrderService orderService)
+        {
+            this.catalogService = catalogService;
+            this.basketService = basketService;
+            this.orderService = orderService;
+        }
+
+        [HttpGet("{userName}", Name = "GetShopping")]
+        [ProducesResponseType(typeof(ShoppingModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingModel>> GetShopping(string userName)
+        {
+            var basket = await basketService.GetBasket(userName);
+
+            foreach (var item in basket.Items)
+            {
+                var product = await catalogService.GetCatalog(item.ProductId);
+
+                // set additional product fields
+                item.ProductName = product.Name;
+                item.Category = product.Category;
+                item.Summary = product.Summary;
+                item.Description = product.Description;
+                item.ImageFile = product.ImageFile;
+            }
+
+            var orders = await orderService.GetOrdersByUserName(userName);
+
+            var shoppingModel = new ShoppingModel
+            {
+                UserName = userName,
+                BasketWithProducts = basket,
+                Orders = orders
+            };
+
+            return Ok(shoppingModel);
+        }
+    }
+}
